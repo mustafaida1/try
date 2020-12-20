b77298a627c530b9d6288e7e3c13b49c076932cf
mustafaida1 <mustafaida@gmail.com>
2020-12-20 06:59:56 +0530

    try it first website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Must from './pic/mu.jpg';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome To My Website</h1>
+    <img src={Must} alt="mustaf" height="300px"></img>
+    </div>
+  );
+}
+
+export default App;
